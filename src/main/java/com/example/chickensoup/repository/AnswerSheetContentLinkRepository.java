bd3359d1275da4a3cf0835c47a8c11f02286bf7a
sxc <1274984171@qq.com>
2022-05-29 22:02:44 +0800

    12

---
+++ src/main/java/com/example/chickensoup/repository/AnswerSheetContentLinkRepository.java
@@ -0,0 +1,7 @@
+package com.example.chickensoup.repository;
+
+import com.example.chickensoup.entity.AnswerSheetContentLinkEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerSheetContentLinkRepository extends JpaRepository<AnswerSheetContentLinkEntity, Integer> {
+}
